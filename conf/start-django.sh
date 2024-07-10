421450e63fabe9c5830818c915b74265f312c63f
junssong <xms2007@naver.com>
2024-07-10 14:13:20 +0900

    Delete: drf

--- conf/start-django.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python manage.py makemigrations
-python manage.py migrate
-python manage.py collectstatic --noinput
-gunicorn --bind 0.0.0.0:8000 --workers 3 --worker-class uvicorn.workers.UvicornWorker pong.asgi:application
