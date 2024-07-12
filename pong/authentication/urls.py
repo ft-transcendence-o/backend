cf4513d73a61c29bde34f5e0c1a7aa1d9350c361
Kim Woo Seok <eypk9673@naver.com>
2024-07-12 14:53:16 +0900

    Style: delete last slash

--- pong/authentication/urls.py
+++ pong/authentication/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from . import auth
 
 urlpatterns = [
-    path('token/exchange/', auth.exchange_access_token, name='exchange_token'),
-    path('user/info/', auth.get_user_info, name='user_info'),
-    path('otp/qrcode/', auth.otp_test, name='otp_qrcode'),
-    path('otp/verify/', auth.verify_otp, name='otp_verify'),
+    path('token/exchange', auth.exchange_access_token, name='exchange_token'),
+    path('user/info', auth.get_user_info, name='user_info'),
+    path('otp/qrcode', auth.otp_test, name='otp_qrcode'),
+    path('otp/verify', auth.verify_otp, name='otp_verify'),
 
     # path("user/", auth.temp_access_token, name="user"),
     # path("user/real", auth.exchange_access_token, name="user_real"),
