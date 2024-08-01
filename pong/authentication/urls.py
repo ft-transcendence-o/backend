de1abaf010974b5cb25048993a4716b5abf732c3
kim woo seok <rladntjr4@gmail.com>
2024-08-02 00:34:18 +0900

    Fix: Name of url

--- pong/authentication/urls.py
+++ pong/authentication/urls.py
@@ -4,7 +4,7 @@ from . import auth
 
 urlpatterns = [
     path('info', auth.UserInfo.as_view(), name='user_info'),
-    path('token', auth.OAuthView.as_view(), name='exchange_token'),
+    path('token', auth.OAuthView.as_view(), name='token'),
     path('otp/qrcode', auth.QRcodeView.as_view(), name='otp_qrcode'),
     path('otp/verify', auth.OTPView.as_view(), name='otp_verify'),
     path('login', auth.Login.as_view(), name='login'),
