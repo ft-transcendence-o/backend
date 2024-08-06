9decf1f55bdcc8c68e3c608dbcd028455b60a8cd
Kim Woo Seok <eypk9673@naver.com>
2024-08-06 17:57:55 +0900

    Feat: Pong game work

--- pong/game/consumers.py
+++ pong/game/consumers.py
@@ -32,13 +32,13 @@ class GameConsumer(WebsocketConsumer):
             self.send(text_data=json.dumps({"game": result}))
 
     def init_game(self):
-        self.ball_pos = np.array([0, 0, 0]) #공위치  #@
-        self.ball_vec = np.array([0, 0, 2]) #공이 움직이는 방향
-        self.ball_rot = np.array([0, 0, 0]) #공의 회전벡터
-        self.angular_vec = np.array([0, 0, 0])
+        self.ball_pos = np.array([0.0, 0.0, 0.0]) #공위치  #@
+        self.ball_vec = np.array([0.0, 1.0, 2.0]) #공이 움직이는 방향
+        self.ball_rot = np.array([0.0, 0.0, 0.0]) #공의 회전벡터
+        self.angular_vec = np.array([0.0, 0.0, 0.0])
         # self.flag = True # 공이 날라가는 방향
-        self.panel1_pos = np.array([0, 0, 50]) #panel1의 초기위치 #@
-        self.panel2_pos = np.array([0, 0, -50]) #panel2의 초기위치 #@
+        self.panel1_pos = np.array([0.0, 0.0, 50.0]) #panel1의 초기위치 #@
+        self.panel2_pos = np.array([0.0, 0.0, -50.0]) #panel2의 초기위치 #@
 
         # 골대쪽 벽면말고 사이드에 있는 4개의 plane들을 의미하며 각각([법선벡터], 원점으로부터의 거리)를 가지고 있다.
         self.planes = [(np.array([1, 0, 0]), 10), (np.array([-1, 0, 0]), 10), (np.array([0, 1, 0]), 10), (np.array([0, -1, 0]), 10)]
@@ -53,10 +53,11 @@ class GameConsumer(WebsocketConsumer):
         # user가 입력한 키값 소켓으로 받기
         steps = 10
         for i in range(steps):
-            movement = np.copy(self.ball_pos) * (0.4 / steps)
+            movement = np.copy(self.ball_vec) * (0.4 / steps)
             # self.ball_rot[0] += self.angular_vec[0]
             # self.ball_rot[1] += self.angular_vec[1]
             # self.ball_rot[2] += self.angular_vec[2]
+            self.ball_pos += movement
 
             collisionPlane = self.collision_with_side()
             if collisionPlane:
@@ -78,6 +79,7 @@ class GameConsumer(WebsocketConsumer):
             if isinstance(collisionPoint, np.ndarray) != False:
                 self.ball_pos = collisionPoint
                 self.ball_pos += (plane[0] * 2) # 현재 공의 좌표에 평면의 법선벡터 * 2를 해서 더해준다
+
                 return plane
     
         return None
@@ -88,13 +90,18 @@ class GameConsumer(WebsocketConsumer):
         ballX, ballY, ballZ = self.ball_pos #공의 좌표
         a, b, c = plane[0] #법선벡터
         d = plane[1] #중심으로부터의 거리
-        return abs(ballX * a + ballY * b + ballZ * c) / math.sqrt(a**2 + b**2 + c**2)
+        # print(f"법선 벡터: {a}, {b}, {c}, {d}")
+        # print(f"현재 평면: {plane[0]}")
+        # print(f"공의 좌표: {ballX}, {ballY}, {ballZ}")
+        # print("거리: ", abs((ballX * a) + (ballY * b) + (ballZ * c) + d) / math.sqrt((a**2) + (b**2) + (c**2)))
+
+        return abs(ballX * a + ballY * b + ballZ * c + d) / math.sqrt(a**2 + b**2 + c**2)
 
 # 구가 평면과 부딪힌 좌표
     def get_collision_point_with_plane(self, plane):
         distance_to_plane = self.plane_distance_to_point(plane)
 
-        if abs(distance_to_plane) < 2:
+        if abs(distance_to_plane) <= 2:
             collisionPoint = self.ball_pos - (plane[0] * distance_to_plane)
             return collisionPoint
         return None
@@ -114,13 +121,13 @@ class GameConsumer(WebsocketConsumer):
     def collisionWithGoalArea(self):
         if self.ball_pos[2] >= 48: #z좌표가 48이상인경우 #player1쪽 벽과 충돌한경우
             # Q: 인자값의 부재
-            if checkBallInPanel(self.panel1_pos):
+            if self.checkBallInPanel(self.panel1_pos): # x,y 좌표 판정
                 self.collisionInPanel(self.panel1_plane) # panel1과 충돌한경우
             else:
                 self.player2Win() # panel1이 위치한 면에 충돌한경우
         elif self.ball_pos[2] <= -48:
             # Q: 인자값의 부재
-            if checkBallInPanel(self.panel2_pos):
+            if self.checkBallInPanel(self.panel2_pos):
                 self.collisionInPanel(self.panel2_plane) # panel2와 충돌한 경우
             else:
                 self.plane1Win()
@@ -140,22 +147,22 @@ class GameConsumer(WebsocketConsumer):
     # panel은 panel의 위치좌표를 의미한다
     def checkBallInPanel(self, panel_pos):
         if abs(self.ball_pos[0] - panel_pos[0]) > 4:
-            return false
+            return False
         elif abs(self.ball_pos[1] - panel_pos[1]) > 4:
-            return false
-        return true
+            return False
+        return True
 
     # 판넬과 공이 충돌한 경우
     def collisionInPanel(self, panelPlane):
         # self.flag = false # 공의 방향을 재설정한다
 
         #충돌지점 계산
-        collisionPoint = get_collision_point_with_plane(panelPlane)
+        collisionPoint = self.get_collision_point_with_plane(panelPlane)
 
         # 충돌후 공의 좌표를 보정
         self.ball_pos = collisionPoint + panelPlane[0] * 2
 
         # angularVec = panelPlane[0] * 0.01
-        updateVector(panelPlane)
+        self.updateVector(panelPlane)
         #updateVectorByPanel()
 
