475ca1ec84ce8b7feb94e803827fe767281e881a
Kim Woo Seok <eypk9673@naver.com>
2024-08-12 16:59:25 +0900

    Docs: Add convention for path

--- pong/game/urls.py
+++ pong/game/urls.py
@@ -2,13 +2,13 @@ from django.urls import path, re_path
 from . import game, views, consumers
 
 
+# BASEURL + /api/game-management/pong-game/
 urlpatterns = [
     path("game", game.GameView.as_view(), name="game"),
     path("tournament", game.TournamentView.as_view(), name="tournament"),
 ]
 
+# BASEURL + /api/pong-game/
 websocket_urlpatterns = [
     path("pong-game/<str:mode>", consumers.GameConsumer.as_asgi(), name="pong_game"),
 ]
-
-# urlpatterns += websocket_urlpatterns
