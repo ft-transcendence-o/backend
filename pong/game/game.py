35bde7f73c8ac6fef1a5e28758897b2752134b58
Kim Woo Seok <eypk9673@naver.com>
2024-08-15 14:26:52 +0900

    Feat: players_name can be default value

--- pong/game/game.py
+++ pong/game/game.py
@@ -161,8 +161,7 @@ class SessionView(View):
 
         user_id = decoded_jwt.get("user_id")
         default_data = get_default_session_data(user_id, "tournament")
-        players_name = body.get("players_name", ["player1", "player2", "player3", "player3"])
-        # TODO: it can be parameter of get_default_session_data
+        players_name = body.get("players_name", default_data.get("players_name"))
         default_data["players_name"] = players_name
         cache.set(f"session_data_tournament_{user_id}", default_data, 500)
         return JsonResponse({"message": "Set session success"})
