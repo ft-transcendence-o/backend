ddb0edc78c2394e68ea48613198eac3daa25234d
kim woo seok <rladntjr4@gmail.com>
2024-08-26 12:53:59 +0900

    Style: Modify import

--- pong/game/urls.py
+++ pong/game/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, re_path
-from . import consumers
+from .consumers import GameConsumer
 from .views import (
     GameView,
     TournamentView,
@@ -9,12 +9,12 @@ from .views import (
 
 # BASEURL + /api/game-management/
 urlpatterns = [
-    path("game", game.GameView.as_view(), name="game"),
-    path("tournament", game.TournamentView.as_view(), name="tournament"),
-    path("session", game.SessionView.as_view(), name="session"),
+    path("game", GameView.as_view(), name="game"),
+    path("tournament", TournamentView.as_view(), name="tournament"),
+    path("session", SessionView.as_view(), name="session"),
 ]
 
 # BASEURL + /api/pong-game/
 websocket_urlpatterns = [
-    path("pong-game/<str:mode>/<int:userid>", consumers.GameConsumer.as_asgi(), name="pong_game"),
+    path("pong-game/<str:mode>/<int:userid>", GameConsumer.as_asgi(), name="pong_game"),
 ]
